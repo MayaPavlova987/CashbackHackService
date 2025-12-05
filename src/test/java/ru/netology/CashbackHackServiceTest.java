54b6239bb0a2fcaf6ce4ad6ee22a02c6779bae27
Pavlova Maya <mayapavlova@yandex.ru>
2025-12-05 17:20:17 +0300

    Change testing

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -19,37 +19,40 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void shouldRemainWhenAmountZero(){
+    public void shouldRemainWhenAmountZero() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 0;
         int expected = 1000;
         int actual = cashbackHackService.remain(amount);
         Assert.assertEquals(actual, expected);
     }
-@Test
-    public void shouldWhenAmountMoreThanBoundary(){
-    CashbackHackService cashbackHackService = new CashbackHackService();
-    int amount = 1500;
-    int expected = 500;
-    int actual = cashbackHackService.remain(amount);
-    Assert.assertEquals(actual, expected);
-}
-@Test
-    public void shouldBoundaryValue(){
-    CashbackHackService cashbackHackService = new CashbackHackService();
-    int amount = 999;
-    int expected = 1;
-    int actual = cashbackHackService.remain(amount);
-    Assert.assertEquals(actual, expected);
-}
-@Test
-    public void shouldWhenAmountEqualsBoundary(){
-    CashbackHackService cashbackHackService = new CashbackHackService();
-    int amount = 1000;
-    int expected = 0;
-    int actual = cashbackHackService.remain(amount);
-    Assert.assertEquals(actual, expected);
-}
+
+    @Test
+    public void shouldWhenAmountMoreThanBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldBoundaryValue() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldWhenAmountEqualsBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        Assert.assertEquals(actual, expected);
+    }
 
 }
 
