1ec25fb199b3a274d1022e0b41a73322ad5406d0
Pavlova Maya <mayapavlova@yandex.ru>
2025-12-05 15:12:10 +0300

    Initial commit

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void cashBackHack(){
+
+    }
+}
